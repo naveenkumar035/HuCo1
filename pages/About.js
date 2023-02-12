55705f0f23daeea495ca28c96265f6e7c82a9b20
naveenkumar035 <gowthamangowthaman1276gmail.com>
2023-02-12 20:27:12 +0530

    initial commit

--- pages/About.js
+++ pages/About.js
@@ -7,13 +7,25 @@ function About() {
         <title>About - HuCo</title>
         <meta name="description" content="Learn more about Conscious Expanding and our mission to help people expand their consciousness." />
       </Head>
-      <h1>About HuCo</h1>
+      <h1>About </h1>
       <p>
         HuCo is a platform dedicated to helping people expand their consciousness and live more fulfilling lives. We believe that by increasing our understanding of ourselves and the world around us, we can create positive change and live more authentic, meaningful lives.
       </p>
       <p>
         Our mission is to provide resources, tools, and guidance to help people on their journey of conscious expansion. We believe that everyone has the potential to live a more conscious, fulfilling life, and we're here to support you on that journey.
       </p>
+      <h2>Exought</h2>
+      <p>
+      "Exought" refers to conscious thoughts that expand beyond their initial form or state.
+      </p>
+      <p>
+      Our mission is to encourage and support the development of conscious expanding thoughts in individuals who seek to deepen their understanding of themselves and the world around them. We believe that expanding one's consciousness leads to greater compassion, empathy, and connection with others, as well as a deeper sense of purpose and fulfillment.
+      </p>
+     {/* <p>
+      We also provide a community forum where members can engage in meaningful discussions, share their experiences, and learn from others. Our community is a safe and inclusive space where all are welcome to participate and contribute.
+
+Thank you for joining us on this journey of conscious expansion. We look forward to sharing and learning with you!
+  </p>*/}
     </div>
   )
 }
