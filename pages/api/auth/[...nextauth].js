9d0edbb3b7637f5eb7fba95bbcd01615432e62a7
naveenkumar035 <gowthamangowthaman1276gmail.com>
2023-02-18 11:45:24 +0530

    initial commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      Secret: process.env.NEXTAUTH_SECRET
     }),
     // ...add more providers here
   ],
