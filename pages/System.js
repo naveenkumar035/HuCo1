7b76f7c48be2ce38b9282f9e819ae35f44e7ec7b
naveenkumar035 <gowthamangowthaman1276gmail.com>
2023-02-19 12:14:30 +0530

    initial commit

--- pages/System.js
+++ pages/System.js
@@ -2,10 +2,13 @@ import Head from "next/head";
 
 function System(){
     return(
+        <>
         <Head>
         <title>HuCo-System</title>
         <meta name="description" content="Learn more about Conscious Expanding and our mission to help people expand their consciousness." />
          </Head>
+         <h1> Stay tuned </h1>
+         </>
     )
 
 }
