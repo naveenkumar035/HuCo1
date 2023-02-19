bfcf226987dfb24c597930917753fefe3c5388bf
naveenkumar035 <gowthamangowthaman1276gmail.com>
2023-02-19 12:10:36 +0530

    initial commit

--- pages/Hub.js
+++ pages/Hub.js
@@ -4,10 +4,13 @@ function Hub(){
     return(
        <>
         <Head>
-        <title>HuCo-Market</title>
+        <title>Hub</title>
         <meta name="description" content="Learn more about Conscious Expanding and our mission to help people expand their consciousness." />
          </Head>
+        <h1>
+          Stay tuned
 
+        </h1>
         
        </>
     )
